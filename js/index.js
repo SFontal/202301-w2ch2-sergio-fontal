925f2c4d5f70f380c021d785dedb7a40e99284e2
SFontal <sergiot.fontal@gmail.com>
2023-01-17 20:20:07 +0100

    Add tests conditionals to strictEquals

--- js/index.js
+++ js/index.js
@@ -5,12 +5,15 @@ console.log(
     "| valueA  | valueB | RESULT |                   |",
     "| ------- | ------ | -------------------------- |",
     `| 1       | 1      | ${strictEquals(1, 1)}   |                   |`,
-    `| NaN     | NaN    | ${strictEquals(NaN, NaN)}  | // Rule Exception |`,
-    `| 0       | -0     | ${strictEquals(0, -0)}   | // Rule Exception |`,
-    `| -0      | 0      | ${strictEquals(-0, 0)}   | // Rule Exception |`,
+    `| NaN     | NaN    | ${strictEquals(NaN, NaN)}   | // Rule Exception |`,
+    `| 0       | -0     | ${strictEquals(0, -0)}  | // Rule Exception |`,
+    `| -0      | 0      | ${strictEquals(-0, 0)}  | // Rule Exception |`,
     `| 1       | "1"    | ${strictEquals(1, "1")}  |                   |`,
     `| true    | false  | ${strictEquals(true, false)}  |                   |`,
-    `| false   | false  | ${strictEquals(false, true)}   |                   |`,
+    `| false   | false  | ${strictEquals(
+      false,
+      false
+    )}   |                   |`,
     `| "Water" | "oil"  | ${strictEquals(
       "Water",
       "oil"
