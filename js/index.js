3d0e27dea63d59d87e477b9630bae8f4f2e5702e
SFontal <sergiot.fontal@gmail.com>
2023-01-18 01:33:28 +0100

    Redo conditionals and ouput

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
 import strictEquals from "./functions/strictEquals.js";
 
-console.log(
-  [
-    "| valueA  | valueB | RESULT |                   |",
-    "| ------- | ------ | -------------------------- |",
-    `| 1       | 1      | ${strictEquals(1, 1)}   |                   |`,
-    `| NaN     | NaN    | ${strictEquals(NaN, NaN)}   | // Rule Exception |`,
-    `| 0       | -0     | ${strictEquals(0, -0)}  | // Rule Exception |`,
-    `| -0      | 0      | ${strictEquals(-0, 0)}  | // Rule Exception |`,
-    `| 1       | "1"    | ${strictEquals(1, "1")}  |                   |`,
-    `| true    | false  | ${strictEquals(true, false)}  |                   |`,
-    `| false   | false  | ${strictEquals(
-      false,
-      false
-    )}   |                   |`,
-    `| "Water" | "oil"  | ${strictEquals(
-      "Water",
-      "oil"
-    )}  |                   |`,
-  ].join("\n")
-);
+console.log(strictEquals("a", "a").join("\n"));
